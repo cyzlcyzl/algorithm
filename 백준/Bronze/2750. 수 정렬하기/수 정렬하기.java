062f4945c26602ef09679c4d79765f16046e2a1c
염혜정 <122238744+cyzlcyzl@users.noreply.github.com>
2023-07-12 16:55:00 +0900

    [Bronze II] Title: 수 정렬하기, Time: 148 ms, Memory: 14708 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -3,13 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    public static void change(int[] arr, int i, int k){
-        int temp = arr[i];
-        arr[i] = arr[k];
-        arr[k] = temp;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -18,11 +11,13 @@ public class Main {
         for(int i = 0; i<arr.length; i++) arr[i] = Integer.parseInt(br.readLine());
         br.close();
 
-        // 버블정렬
-        for(int i = 0; i<arr.length; i++){
-            for(int k = 0; k<i; k++){
-                if(arr[i]<arr[k]) change(arr, i, k);
+        // 선택정렬
+        for(int i = 0; i<N-1; i++){
+        int minIdx = i;
+            for(int k = i+1; k<N; k++){
+                if(arr[minIdx]>arr[k]) minIdx = k;
             }
+                change(arr, minIdx, i);
         }
 
         StringBuilder sb = new StringBuilder();
@@ -30,5 +25,13 @@ public class Main {
 
         sb.setLength(sb.length() - 1);
         System.out.println(sb);
+
     }
+
+    public static void change(int[] arr, int i, int k){
+        int temp = arr[i];
+        arr[i] = arr[k];
+        arr[k] = temp;
+    }
+
 }
