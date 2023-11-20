0059a30fb3ac820fbe531c36662bba88fd80efa8
염혜정 <122238744+cyzlcyzl@users.noreply.github.com>
2023-11-20 13:25:37 +0900

    [Silver I] Title: 점프, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -4,31 +4,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    
+    static int N;
     static int[][] arr;
     static long[][] dp;
-
+    
+    static void jump(){
+        dp[0][0] = 1;
+        
+        for(int i = 0; i<N; i++){
+            for(int k = 0; k<N; k++){
+                int val = arr[i][k];
+                if(val == 0) break;
+                if(i + val < N) dp[i+val][k] += dp[i][k];
+                if(k + val < N) dp[i][k+val] += dp[i][k];
+            }
+        }
+    }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        
+        N = Integer.parseInt(br.readLine());
         arr = new int[N][N];
         dp = new long[N][N];
-
+        
         for(int i = 0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             for(int k = 0; k<N; k++){
                 arr[i][k] = Integer.parseInt(st.nextToken());
             }
         }
-        dp[0][0] = 1;
-
-        for(int i = 0; i<N; i++){
-            for(int k = 0; k<N; k++){
-                int val = arr[i][k];
-                if(val == 0) continue;
-                if(k + val < N) dp[i][k+val] += dp[i][k];
-                if(i + val < N) dp[i+val][k] += dp[i][k];
-            }
-        }
+        
+        jump();
         System.out.println(dp[N-1][N-1]);
     }
 }
