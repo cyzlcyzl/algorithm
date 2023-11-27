8cd7adbceb6e80d90b1f6f8b395c175c898ba8eb
염혜정 <122238744+cyzlcyzl@users.noreply.github.com>
2023-11-27 11:08:24 +0900

    [Silver IV] Title: 듣보잡, Time: 296 ms, Memory: 26384 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,37 +1,32 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		Set<String> unHeared = new HashSet<>();
+		Set<String> unLooked = new HashSet<>();
+		for(int i = 0; i<N; i++) unLooked.add(br.readLine());
 		
-		for(int i = 0; i<N; i++){
-		    unHeared.add(br.readLine());
-		}
-		
-		List<String> unHearedAndLooked = new ArrayList<>();
-		
+		List<String> unLookedAndunHeared = new ArrayList<>();
 		for(int i = 0; i<M; i++){
-		    String input = br.readLine();
-		    if(unHeared.contains(input)) unHearedAndLooked.add(input);
+		    String name = br.readLine();
+		    if(unLooked.contains(name)) unLookedAndunHeared.add(name);
 		}
 		
-		Collections.sort(unHearedAndLooked);
-		StringBuilder sb = new StringBuilder();
-		sb.append(unHearedAndLooked.size()).append("\n");
+		Collections.sort(unLookedAndunHeared);
 		
-		for(String name : unHearedAndLooked){
+		StringBuilder sb = new StringBuilder();
+		sb.append(unLookedAndunHeared.size()).append("\n");
+		for(String name : unLookedAndunHeared) {
 		    sb.append(name).append("\n");
 		}
 		
 		sb.setLength(sb.length()-1);
 		System.out.println(sb);
+		
 	}
 }
