dd6bae760ca93d7fca00e2d64a7b5d5775001ab8
염혜정 <122238744+cyzlcyzl@users.noreply.github.com>
2023-12-03 01:24:19 +0900

    [Silver II] Title: 부분수열의 합, Time: 212 ms, Memory: 15580 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,49 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    static int N, S, cnt, sum;
+public class Main{
+    
+    static int N, S;
     static int[] arr;
-    static boolean[] isSelected;
-
-    static void subset(int size){
-        // 종료 조건
-        if(size == N){
-            for(int i = 0; i<N; i++){
-                if(isSelected[i]) sum += arr[i];
+    static boolean[] isVisited;
+    
+    static int subset(int depth){
+        if(depth == N){
+            int sum = 0;
+            for(int i = 0; i<arr.length; i++){
+                if(! isVisited[i]) continue;
+                sum += arr[i];
             }
-            if(sum==S) cnt++;
-            sum = 0;
-            return;
+            if(sum == S) return 1;
+            return 0;
         }
-
-        // 확장
-        isSelected[size] = true;
-        subset(size + 1);
-        isSelected[size] = false;
-        subset(size + 1);
+        
+        isVisited[depth] = true;
+        int case1 = subset(depth + 1);
+        isVisited[depth] = false;
+        int case2 = subset(depth + 1);
+        
+        return case1 + case2;
     }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N= Integer.parseInt(st.nextToken());
-        S= Integer.parseInt(st.nextToken());
-        arr = new int[N];
-        isSelected = new boolean[N];
-        cnt = 0;
-        sum = 0;
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
-        br.close();
-
-        // 부분수열
-        subset(0);
-        if(S==0) cnt--;
-        System.out.println(cnt);
-
-    }
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		isVisited = new boolean[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		
+		int answer = subset(0);
+		if(S == 0) answer--;
+		System.out.println(answer);
+	}
 }
