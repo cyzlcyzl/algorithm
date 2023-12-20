f6440961bc9610730fb01c333a285c5b676893c1
염혜정 <122238744+cyzlcyzl@users.noreply.github.com>
2023-12-20 23:40:50 +0900

    [Gold V] Title: 트리, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,43 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main{
-
+    
     static int N;
     static ArrayList<Integer>[] tree;
-    static int root;
-    static int remove;
-    static int count;
-
-    static void dfs(int node){
-        if(node == remove) return;
-        if(tree[node].size() == 0) count++;
-        for(int n : tree[node]) dfs(n);
+    static int root, removeNum;
+    static int cnt = 0;
+    
+    static void dfs(int start){
+        if(start == removeNum) return;
+        if(tree[start].size() == 0) cnt++;
+        for(int next : tree[start]) dfs(next);
     }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        tree = new ArrayList[N];
-        for(int i = 0; i<tree.length; i++) tree[i] = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0 ; i<tree.length; i++){
-            int parent = Integer.parseInt(st.nextToken());
-            if(parent == -1) {
-                root = i;
-                continue;
-            }
-            tree[parent].add(i);
-        }
-        remove = Integer.parseInt(br.readLine());
-        count = 0;
-        if(root != remove){
-            for(int i = 0; i<tree.length; i++) tree[i].remove(Integer.valueOf(remove));
-            dfs(root);
-        }
-        System.out.println(count);
-    }
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		tree = new ArrayList[N];
+		for(int i = 0; i<N; i++) tree[i] = new ArrayList<>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i<N; i++){
+		    int p = Integer.parseInt(st.nextToken());
+		    if(p != -1) tree[p].add(i);
+		    else root = i;
+		}
+		
+		removeNum = Integer.parseInt(br.readLine());
+		
+		if(root != removeNum){
+		    for(int i = 0; i<N; i++) tree[i].remove(Integer.valueOf(removeNum));
+		    dfs(root);
+		}
+		System.out.println(cnt);
+	}
 }
